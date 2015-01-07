becc957c1c64ac07233a742a264ee1271c5c11a4
Cambridge Yang <jq.yang@berkeley.edu>
2015-01-07 22:16:05 +0800

    update docstring

--- tastypie_oauth/authentication.py
+++ tastypie_oauth/authentication.py
@@ -103,14 +103,14 @@ class OAuth2Scoped0Authentication(OAuth20Authentication):
             get, post, patch and put is desired to be a scope or a list of scopes or None
             if get is None, it will default to post
             if delete is None, it will default to post
-            if both patch and put are none, they are all default to post
-            if one of patch or put is none, the two will default to the one that is not none
+            if both patch and put are None, they are all default to post
+            if one of patch or put is None, the two will default to the one that is not None
 
             You can turn this overriding behavior off entirely by specifying use_default=False, but then remember that None means no scope requirement
             is specified for that http method
 
             the list of scopes should have a logic "or" between them
-            e.g. get=("a b","c") for oauth2-toolkit means "an HTTP GET method requires scope "a b"("a" and "b") or scope "c"
+            e.g. get=("a b","c") for oauth2-toolkit means "GET method requires scope 'a b'('a' and 'b') or scope 'c' "
                  get=(a|b,c) is the corresponding form for oauth2-provider, where a,b,c should be some constans you defined in your settings
                  Note: for oauth2-toolkit, you have to provide a space seperated string of combination of scopes
             you can also specify only one scope(instead of a list), and that scope will the only scope that has permission to the according method
