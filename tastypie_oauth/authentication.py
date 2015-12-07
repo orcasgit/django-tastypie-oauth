20c4f4ad91b8829cc853e4a37aaf3a52ac78f9ce
Marcelo Jorge Vieira <metal@alucinados.com>
2015-12-07 11:25:40 -0200

    Fixed is_authenticated()
    Error: 'NoneType' object has no attribute 'has_perm'. It happens when a
    'client_credentials' is used to POST a resource URL.

--- tastypie_oauth/authentication.py
+++ tastypie_oauth/authentication.py
@@ -63,7 +63,7 @@ class OAuth20Authentication(Authentication):
 
             # If OAuth authentication is successful, set the request user to
             # the token user for authorization
-            request.user = token.user
+            request.user = token.user or AnonymousUser()
 
             # If OAuth authentication is successful, set oauth_consumer_key on
             # request in case we need it later
